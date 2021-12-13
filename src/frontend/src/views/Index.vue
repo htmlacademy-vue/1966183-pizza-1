61ba91fc565a5a8d5bbd6dd8f15c8a1ce9af2f35
rradaykin <rradaykin@phoenixit.ru>
2021-12-13 10:49:42 +0300

    add layout

---
+++ src/frontend/src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HeaderLayout></HeaderLayout>
+    <Constructor></Constructor>
+  </div>
+</template>
+
+<script>
+import HeaderLayout from "/src/layouts/HeaderLayout.vue";
+import Constructor from "/src/layouts/Constructor.vue";
+export default {
+  name: "Index",
+  components: { HeaderLayout, Constructor },
+};
+</script>
+
+<style lang="scss" scoped></style>
