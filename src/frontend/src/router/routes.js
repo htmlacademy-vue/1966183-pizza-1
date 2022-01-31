f1278bc219b4362f65c1e2b5bd7434049552454a
rradaykin <rradaykin@phoenixit.ru>
2022-01-31 23:17:26 +0300

    fix

--- src/frontend/src/router/routes.js
+++ src/frontend/src/router/routes.js
@@ -1,62 +1,42 @@
 export const routes = [
-  // {
-  //   path: "/cart",
-  //   name: "Cart",
-  //   component: Cart,
-  // },
   {
-    path: "/cart", // url
-    name: "Cart", // наименование маршрута
-    component: () => import("../views/Cart.vue"), // компонент для монтирования
+    path: "/cart",
+    name: "Cart",
+    component: () => import("../views/Cart.vue"),
     meta: {
-      layout: "AppLayoutDefault", // применяемый шаблон
-    },
-  },
-  // {
-  //   path: "/orders",
-  //   name: "Orders",
-  //   component: Orders,
-  // },
-  {
-    path: "/orders", // url
-    name: "Orders", // наименование маршрута
-    component: () => import("../views/Orders.vue"), // компонент для монтирования
-    meta: {
-      layout: "AppLayoutMain", // применяемый шаблон
-    },
-  },
-  // {
-  //   path: "/profile",
-  //   name: "Profile",
-  //   component: Profile,
-  // },
-  {
-    path: "/profile", // url
-    name: "Profile", // наименование маршрута
-    component: () => import("../views/Profile.vue"), // компонент для монтирования
-    meta: {
-      layout: "AppLayoutMain", // применяемый шаблон
-    },
-  },
-  // {
-  //   path: "/login",
-  //   name: "Login",
-  //   component: Login,
-  // },
-  {
-    path: "/login", // url
-    name: "Login", // наименование маршрута
-    component: () => import("../views/Login.vue"), // компонент для монтирования
-    meta: {
-      layout: "AppLayoutClear", // применяемый шаблон
+      layout: "AppLayoutDefault",
     },
   },
   {
-    path: "/", // url
-    name: "IndexHome", // наименование маршрута
-    component: () => import("../views/Index.vue"), // компонент для монтирования
+    path: "/orders",
+    name: "Orders",
+    component: () => import("../views/Orders.vue"),
     meta: {
-      layout: "AppLayoutDefault", // применяемый шаблон
+      layout: "AppLayoutMain",
+    },
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: () => import("../views/Profile.vue"),
+    meta: {
+      layout: "AppLayoutMain",
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+    meta: {
+      layout: "AppLayoutClear",
+    },
+  },
+  {
+    path: "/",
+    name: "IndexHome",
+    component: () => import("../views/Index.vue"),
+    meta: {
+      layout: "AppLayoutDefault",
     },
   },
 ];
