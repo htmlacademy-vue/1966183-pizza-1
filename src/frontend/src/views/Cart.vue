ed24b5edfe5a459848f90606f118d015a523c8d6
rradaykin <rradaykin@phoenixit.ru>
2022-02-03 22:24:28 +0300

    done

--- src/frontend/src/views/Cart.vue
+++ src/frontend/src/views/Cart.vue
@@ -11,75 +11,17 @@
         </div>
 
         <ul class="cart-list sheet" v-else>
-          <li
-            class="cart-list__item"
+          <CartPizzaAndCounterItem
             v-for="(pizza, index) in correctPizzasFormat"
-            :key="index"
-          >
-            <div class="product cart-list__product">
-              <img
-                src="@/assets/img/product.svg"
-                class="product__img"
-                width="56"
-                height="56"
-                alt="Капричоза"
-              />
-              <div class="product__text">
-                <h2>
-                  {{ pizza.name === "" ? "Названия нет :(" : pizza.name }}
-                </h2>
-                <ul>
-                  <li>{{ pizza.size }}, {{ pizza.dough }}</li>
-                  <li>Соус: {{ pizza.sauce }}</li>
-                  <li>Начинка: {{ pizza.ingredients }}</li>
-                </ul>
-              </div>
-            </div>
-
-            <div class="counter cart-list__counter">
-              <button
-                type="button"
-                class="counter__button counter__button--minus"
-                @click="incrementPizzaCount(pizza.id)"
-              >
-                <span class="visually-hidden">Меньше</span>
-              </button>
-              <input
-                type="text"
-                name="counter"
-                class="counter__input"
-                :value="pizzasCountAndPrice[pizza.id].count"
-              />
-              <button
-                type="button"
-                class="counter__button counter__button--plus counter__button--orange"
-                @click="$store.commit('Cart/incCountPizza', pizza.id)"
-              >
-                <span class="visually-hidden">Больше</span>
-              </button>
-            </div>
-
-            <div class="cart-list__price">
-              <b>{{ pizza.price }} ₽</b>
-            </div>
-
-            <div class="cart-list__button">
-              <router-link to="/">
-                <button
-                  type="button"
-                  class="cart-list__edit"
-                  @click="
-                    $store.commit(
-                      'Builder/setBuilderToChange',
-                      pizzasInBasket[index]
-                    )
-                  "
-                >
-                  Изменить
-                </button>
-              </router-link>
-            </div>
-          </li>
+            :key="pizza.id"
+            :pizza="pizza"
+            :index="index"
+            :pizzas-in-basket="pizzasInBasket"
+            :pizzas-count-and-price="pizzasCountAndPrice"
+            @changePizza="$store.commit('Builder/setBuilderToChange', $event)"
+            @incButtonClick="$store.commit('Cart/incCountPizza', $event)"
+            @decButtonClick="incrementPizzaCount($event)"
+          />
         </ul>
 
         <div class="cart__additional">
@@ -134,23 +76,8 @@
         />
       </div>
     </main>
-    <section class="footer">
-      <div class="footer__more">
-        <a href="#" class="button button--border button--arrow"
-          >Хочу еще одну</a
-        >
-      </div>
-      <p class="footer__text">
-        Перейти к конструктору<br />чтоб собрать ещё одну пиццу
-      </p>
-      <div class="footer__price">
-        <b>Итого: 2 228 ₽</b>
-      </div>
-
-      <div class="footer__submit">
-        <button type="submit" class="button">Оформить заказ</button>
-      </div>
-    </section>
+    <CartFooter :final-price="finalPrice" @show-popup="isPopupOpen = true" />
+    <CartOrderPlaced v-if="isPopupOpen" @close-popup="closePopup" />
   </form>
 </template>
 
@@ -159,10 +86,19 @@ import { mapGetters, mapMutations } from "vuex";
 import { getSauce, getSize, getDough } from "@/common/helpers/classes";
 import CartAdditionalProduct from "../modules/cart/CartAdditionalProduct";
 import CartClientsInfo from "../modules/cart/CartClientsInfo";
+import CartOrderPlaced from "../modules/cart/CartOrderPlaced";
+import CartFooter from "../modules/cart/CartFooter";
+import CartPizzaAndCounterItem from "../modules/cart/CartPizzaAndCounterItem";
 
 export default {
   name: "Cart",
-  components: { CartAdditionalProduct, CartClientsInfo },
+  components: {
+    CartAdditionalProduct,
+    CartClientsInfo,
+    CartOrderPlaced,
+    CartFooter,
+    CartPizzaAndCounterItem,
+  },
   data() {
     return {
       types: [
@@ -170,6 +106,7 @@ export default {
         { name: "sauce", description: "Острый соус" },
         { name: "potato", description: "Картошка из печи" },
       ],
+      isPopupOpen: false,
     };
   },
   computed: {
@@ -213,6 +150,9 @@ export default {
       if (this.pizzasCountAndPrice[id].count === 0) {
         this.deletePizza(id);
       }
+    },
+    closePopup() {
+      this.isPopupOpen = false;
     },
   },
 };
